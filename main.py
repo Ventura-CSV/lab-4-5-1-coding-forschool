530397e2d45f70aafdca8842345cf557d1d34b1c
Ethan Wang <ethan_wang2@my.vcccd.edu>
2024-06-28 03:09:06 -0700

    First

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import random
 
 
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+
+    numbers = []
+    total = 0
+    i = 0
+    while total < 5:
+        num = random.random(0,100)
+        numbers.append(num)
+        i += 1
+
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
